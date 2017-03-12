704d317f00441f1f72b6fbe9ec2c5800002fea4f
Daniel Atkinson <hourd.tasa@gmail.com>
2017-03-12 14:19:17 +0000

    Added setup script to install Pi TFT

---
+++ setup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+echo "Updating package definitions"
+sudo apt update
+echo "Upgrading all currently installed packages"
+sudo apt upgrade -y --force-yes
+echo "Adding Adafruit package repository"
+curl -SLs https://apt.adafruit.com/add-pin | sudo bash
+echo "Installing dependancies for Pi TFT"
+sudo apt-get install -y --force-yes raspberrypi-bootloader adafruit-pitft-helper raspberrypi-kernel
+echo "Setting up Pi TFT"
+echo "Please follow the on-screen prompts"
+sleep 2
+sudo adafruit-pitft-helper -t 28r
+echo "You may need to reboot to enable the Pi TFT"
