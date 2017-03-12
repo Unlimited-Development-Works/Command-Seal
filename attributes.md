a93c232979e92a66b41397eaf3d38c30056bfbd7
Daniel Atkinson <hourd.tasa@gmail.com>
2017-03-12 15:07:15 +0000

    basic pygame ui to display hostname and IP

---
+++ attributes.md
@@ -0,0 +1,4 @@
+# Attributes
+
+
+The pygame user interface is heavily inspired by https://github.com/garthvh/pitftmenu
---
+++ menu.py
@@ -0,0 +1,85 @@
+
+import sys, pygame, socket
+from pygame.locals import *
+import time
+import subprocess
+import os
+import RPi.GPIO
+from subprocess import *
+os.environ["SDL_FBDEV"] = "/dev/fb1"
+os.environ["SDL_MOUSEDEV"] = "/dev/input/touchscreen"
+os.environ["SDL_MOUSEDRV"] = "TSLIB"
+
+# Initialize pygame and hide mouse
+pygame.init()
+pygame.mouse.set_visible(0)
+
+# colors    R    G    B
+white   = (255, 255, 255)
+red     = (255,   0,   0)
+green   = (  0, 255,   0)
+blue    = (  0,   0, 255)
+black   = (  0,   0,   0)
+cyan    = ( 50, 255, 255)
+magenta = (255,   0, 255)
+yellow  = (255, 255,   0)
+orange  = (255, 127,   0)
+
+#set size of the screen
+size = width, height = 320, 240
+screen = pygame.display.set_mode(size)
+
+# Background Color
+screen.fill(black)
+
+# Run a custom command
+def run_cmd(cmd):
+    process = Popen(cmd.split(), stdout=PIPE)
+    output = process.communicate()[0]
+    return output
+
+# Get Your External IP Address
+def get_ip():
+    ip_msg = "Not connected"
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        s.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+        s.connect(('<broadcast>', 0))
+        ip_msg="IP:" + s.getsockname()[0]
+    except Exception:
+        pass
+    return ip_msg
+
+# define function that checks for touch location
+def on_touch():
+    # get the position that was touched
+    touch_pos = (pygame.mouse.get_pos() [0], pygame.mouse.get_pos() [1])
+    print touch_pos
+
+# Outer Border
+pygame.draw.rect(screen, orange, (0,0,320,240),10)
+pi_hostname = run_cmd("hostname")
+pi_hostname = pi_hostname[:-1]
+
+# define function for printing text in a specific place with a specific colour
+def make_label(text, xpo, ypo, fontsize, colour):
+    font=pygame.font.Font(None,fontsize)
+    label=font.render(str(text), 1, (colour))
+    screen.blit(label,(xpo,ypo))
+
+# Buttons and labels
+# First Row Label
+make_label(pi_hostname + " - " +  get_ip(), 15, 15, 28, orange)
+
+#While loop to manage touch screen inputs
+while 1:
+    for event in pygame.event.get():
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            pos = (pygame.mouse.get_pos() [0], pygame.mouse.get_pos() [1])
+            on_touch()
+
+        #ensure there is always a safe way to end the program if the touch screen fails
+        if event.type == KEYDOWN:
+            if event.key == K_ESCAPE:
+                sys.exit()
+    pygame.display.update()
